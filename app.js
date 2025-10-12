48dad1ea7b67d2a2a65cdee1ed50e32d9e8bada2
Jonathan da Silva <jonathansilva15sp@gmail.com>
2025-10-11 21:35:19 -0300

    feat: adicionar chave de segurança

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./controllers/bebidaController.js";
+import { apiKeyMiddleware } from "./config/auth.js"
 
 const app = express();
 
 app.use(express.json());
+app.use(apiKeyMiddleware)
 app.use("/bebidas", router);
 
 export default app;
